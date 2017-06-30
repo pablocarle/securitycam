09c83f914724687e96d11fb019deda967e70318f
Pablo Carle <pablo.carle@gmail.com>
2017-06-29 22:00:38 -0300

    Removed unused classes

--- cam-manager/src/main/java/com/sgrvg/security/OptionsCommandHandler.java
+++
@@ -1,7 +0,0 @@
-package com.sgrvg.security;
-
-import io.netty.channel.ChannelInboundHandlerAdapter;
-
-public class OptionsCommandHandler extends ChannelInboundHandlerAdapter {
-
-}
--- cam-manager/src/main/java/com/sgrvg/security/SetupCommandHandler.java
+++
@@ -1,5 +0,0 @@
-package com.sgrvg.security;
-
-public class SetupCommandHandler {
-
-}
